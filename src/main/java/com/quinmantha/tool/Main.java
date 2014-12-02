011c511ffaa2c91df5e91d586f95d5ff53eca28c
Hiroki Tsukahara <asuka@quinmantha.com>
2014-12-02 21:30:21 +0900

    prepare 3panes for file.

--- src/main/java/com/quinmantha/tool/Main.java
+++ src/main/java/com/quinmantha/tool/Main.java
@@ -2,7 +2,10 @@ package com.quinmantha.tool;
 
 import net.sourceforge.plantuml.FileFormat;
 import net.sourceforge.plantuml.FileFormatOption;
+import net.sourceforge.plantuml.GeneratedImage;
+import net.sourceforge.plantuml.SourceFileReader;
 import net.sourceforge.plantuml.SourceStringReader;
+import net.sourceforge.plantuml.preproc.Defines;
 
 import org.apache.batik.dom.svg.SAXSVGDocumentFactory;
 import org.apache.batik.swing.JSVGCanvas;
@@ -23,6 +26,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -42,26 +46,43 @@ A<|--B
 @enduml
  */
 public class Main extends UpdateCheck {
+    public static void main(String[] args){
+        String targetName = args[0];
+        System.out.println("watch:"+args[0]);
+        Main obj = new Main(targetName);
+        obj.start();
+    }
+
+
+    private ArrayList<JSVGCanvas> canvas;
+    private JFrame viewFrame;
     public Main(String target){
         super(new File(target));
+
+        viewFrame = new JFrame();
+
+        viewFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        viewFrame.getContentPane().setBackground(Color.WHITE);
+        viewFrame.getContentPane().setLayout(new FlowLayout(FlowLayout.LEFT,32,32));
+        viewFrame.setSize(400,300);
+
+        canvas = new ArrayList<JSVGCanvas>();
+
+        canvas.add(new JSVGCanvas());
+        canvas.get(0).setBackground(new Color(255,250,205));
+        viewFrame.getContentPane().add(canvas.get(0));
+
+        canvas.add(new JSVGCanvas());
+        canvas.get(1).setBackground(new Color(255,250,205));
+        viewFrame.getContentPane().add(canvas.get(1));
+
+        canvas.add(new JSVGCanvas());
+        canvas.get(2).setBackground(new Color(255,250,205));
+        viewFrame.getContentPane().add(canvas.get(2));
+
+        viewFrame.setVisible(true);
     }
 
-
-/*
-    private static void openSVG(File f){
-        try {
-            String parser = XMLResourceDescriptor.getXMLParserClassName();
-            SAXSVGDocumentFactory svgf = new SAXSVGDocumentFactory(parser);
-            InputStream is = new FileInputStream(f);
-            Document doc = svgf.createDocument(null,is);
-            canvas.setDocument(doc);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-*/
     private boolean is(File f, String ext){
         String fname = f.getName();
         String t = "";
@@ -72,55 +93,11 @@ public class Main extends UpdateCheck {
         return t.equalsIgnoreCase(ext);
     }
 
-    static ArrayList<JSVGCanvas> canvas = new ArrayList<JSVGCanvas>();
-    public static void main(String[] args){
-        String targetName = args[0];
-        System.out.println("watch:"+args[0]);
-
-        Main obj = new Main(targetName);
-        obj.start();
-
-        JFrame f = new JFrame();
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.getContentPane().setBackground(Color.WHITE);
-        f.getContentPane().setLayout(new FlowLayout());
-
-        canvas.add(new JSVGCanvas());
-        f.getContentPane().add(canvas.get(0));
-        canvas.add(new JSVGCanvas());
-        f.getContentPane().add(canvas.get(1));
-
-        /*
-        JMenuBar bar = new JMenuBar();
-        JMenu menu = new JMenu("File");
-        bar.add(menu);
-        JMenuItem out = new JMenuItem("Open SVG");
-        menu.add(out);
-        out.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser chooser = new JFileChooser();
-                int ret = chooser.showDialog(null, "Select File");
-                if(ret!=JFileChooser.APPROVE_OPTION){
-                    return ;
-                }
-                File file = chooser.getSelectedFile();
-                openSVG(file);
-            }
-        });
-        f.setJMenuBar(bar);
-        */
-        f.setSize(400,300);
-        f.setVisible(true);
-    }
 
     @Override
     protected void onModified(String filename){
         System.out.println("update svg (" + filename + ")");
         File f = new File(getTargetDir(), filename);
-        //if(is(f,"svg")) {
-        //openSVG(f);
-        //} else
 
         if( filename.charAt(0) != '.' ) {
             if (is(f, "puml")) {
@@ -134,43 +111,21 @@ public class Main extends UpdateCheck {
     }
 
     private void openPlantUML(File f) throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        FileInputStream in = new FileInputStream(f);
-        byte buffer[] = new byte[4096];
-        int n;
-        while((n=in.read(buffer)) >= 0){
-            out.write(buffer,0,n);
-        }
-        String s = out.toString("utf-8");
-        SourceStringReader reader = new SourceStringReader(s,"utf-8");
+        SourceFileReader sfReader
+                = new SourceFileReader(new Defines(), f, null, new LinkedList<String>(), "utf-8", new FileFormatOption(FileFormat.SVG));
 
-        List<String> svgs = new LinkedList<String>();
-        int idx=0;
-        for(;;) {
-            ByteArrayOutputStream svg = new ByteArrayOutputStream();
-            String res = reader.generateImage(svg, idx, new FileFormatOption(FileFormat.SVG));
-            if(res == null){
-                break;
-            }
-            System.out.println(res);
-            svgs.add(new String(svg.toByteArray(), "utf-8"));
-            svg.close();
-            System.out.println(String.format("output #%d",idx));
-            idx++;
-        }
+        Collection<GeneratedImage> images = sfReader.getGeneratedImages();
 
         String parser = XMLResourceDescriptor.getXMLParserClassName();
         SAXSVGDocumentFactory svgf = new SAXSVGDocumentFactory(parser);
-        int i=0;
-        for(String svg : svgs) {
-            Document doc = svgf.createDocument(null, new StringReader(svg));
-            canvas.get(i).setDocument(doc);
-            i++;
-            if( canvas.size() <= i){
+        int idx = 0;
+        for(GeneratedImage g : images){
+            Document doc = svgf.createDocument(null, new FileInputStream(g.getPngFile()));
+            canvas.get(idx).setDocument(doc);
+            idx++;
+            if( idx >= canvas.size()){
                 break;
             }
         }
     }
-
-
 }
